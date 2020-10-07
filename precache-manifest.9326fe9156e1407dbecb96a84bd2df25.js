5cf691bc456ac0242631e82b9b15e497ae8ad072
nahid-pavel <pavelavw@yahoo.com>
2020-10-07 13:16:51 +0600

    Updates

---
+++ precache-manifest.9326fe9156e1407dbecb96a84bd2df25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb46b24f35925825b037e843eac42d22",
+    "url": "/react-youtube-fetch/index.html"
+  },
+  {
+    "revision": "763e73404095ad349263",
+    "url": "/react-youtube-fetch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d42cf312e73b1eb8e97a",
+    "url": "/react-youtube-fetch/static/js/2.13860ffa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-youtube-fetch/static/js/2.13860ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763e73404095ad349263",
+    "url": "/react-youtube-fetch/static/js/main.e176eb68.chunk.js"
+  },
+  {
+    "revision": "148e38ad7e57a6171841",
+    "url": "/react-youtube-fetch/static/js/runtime-main.c42fe6a8.js"
+  }
+]);
